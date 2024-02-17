17afa4ad7f55be795e78daa32d8dac52eaa12c44
@codewithDevWebPro <115881946+Moeen-Kazmi369@users.noreply.github.com>
2024-02-17 15:48:20 -0800

    moeen

--- src/client/components/patients/dashboard/notification.jsx
+++ src/client/components/patients/dashboard/notification.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { client_01, client_02, client_03, client_04 } from '../../imagepath';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -9,11 +9,11 @@ function Notification() {
     try {
       const response = await axios.get(`http://localhost:3005/api/mydoctor/${userId}`);
       setDocAppointment(response.data);
-     
-     
+
+
     } catch (error) {
       console.error('Error fetching appointments:', error);
-    
+
     }
   };
   useEffect(() => {
@@ -31,99 +31,82 @@ function Notification() {
         >
           <i className="fa-solid fa-bell text-white" /> <span className="badge">{docAppointment && docAppointment.length}</span>
         </Link>
-        <div className="dropdown-menu notifications dropdown-menu-end ">
-          {/* <div className="topnav-dropdown-header">
-            <span className="notification-title">Notifications</span>
-          </div> */}
+        <div style={{
+          top: "50px"
+        }} className="dropdown-menu py-3 px-0   rounded-2 notifications dropdown-menu-end ">
+          <div className="topnav-dropdown-header w-100 border-dark-subtle  p-2">
+            <span style={{
+              color:"gray"
+            }} className='fw-bold fs-4 '>Notifications</span>
+          </div>
           <div className="noti-content">
             <ul className="notification-list">
-              <li className="notification-message">
+              <li className="notification-message px-3 my-1  border-dark-subtle  ">
                 <Link to="/patient/dashboard">
                   <div className="media d-flex">
-                    {/* <span className="avatar">
-                      <img
-                        className="avatar-img"
-                        alt=""
-                        src={client_01}
-                      />
-                    </span> */}
-                    <div className="media-body">
-                      <h6>
+                    <div style={{
+                      color: "gray"
+                    }} className="d-flex flex-column justify-content-center py-1  align-items-start ">
+                      <h6 style={{
+                        color: "gray"
+                      }} className='w-100  d-flex justify-content-between align-items-center  fs-5 '>
                         Travis Tremble{" "}
-                        <span className="notification-time">18.30 PM</span>
+                        <span className='p-1 bg-success  rounded-circle '></span>
                       </h6>
-                      <p className="noti-details">
+                      <p style={{
+                        fontWeight: "normal"
+                      }} className="fw-light">
                         Sent a amount of $210 for his Appointment{" "}
-                        <span className="noti-title">Dr.Ruby perin </span>
+                        <span className="">Dr.Ruby perin </span>
                       </p>
+                      <span className="">18.30 PM</span>
                     </div>
                   </div>
                 </Link>
               </li>
-              <li className="notification-message">
+
+              <li className="notification-message px-3 my-1  border-dark-subtle  ">
                 <Link to="/patient/dashboard">
                   <div className="media d-flex">
-                    {/* <span className="avatar">
-                      <img
-                        className="avatar-img"
-                        alt=""
-                        src={client_02}
-                      />
-                    </span> */}
-                    <div className="media-body">
-                      <h6>
+                    <div style={{
+                      color: "gray"
+                    }} className="d-flex flex-column justify-content-center py-1  align-items-start ">
+                      <h6 style={{
+                        color: "gray"
+                      }} className='w-100  d-flex justify-content-between align-items-center  fs-5 '>
                         Travis Tremble{" "}
-                        <span className="notification-time">12 Min Ago</span>
+                        <span className='p-1 bg-success  rounded-circle '></span>
                       </h6>
-                      <p className="noti-details">
-                        {" "}
-                        has booked her appointment to{" "}
-                        <span className="noti-title">Dr. Hendry Watt</span>
+                      <p style={{
+                        fontWeight: "normal"
+                      }} className="fw-light">
+                        Sent a amount of $210 for his Appointment{" "}
+                        <span className="">Dr.Ruby perin </span>
                       </p>
+                      <span className="">18.30 PM</span>
                     </div>
                   </div>
                 </Link>
               </li>
-              <li className="notification-message">
+              <li className="notification-message px-3 my-1  border-dark-subtle  ">
                 <Link to="/patient/dashboard">
                   <div className="media d-flex">
-                    {/* <div className="avatar">
-                      <img
-                        className="avatar-img"
-                        alt=""
-                        src={client_03}
-                      />
-                    </div> */}
-                    <div className="media-body">
-                      <h6>
+                    <div style={{
+                      color: "gray"
+                    }} className="d-flex flex-column justify-content-center py-1  align-items-start ">
+                      <h6 style={{
+                        color: "gray"
+                      }} className='w-100  d-flex justify-content-between align-items-center  fs-5 '>
                         Travis Tremble{" "}
-                        <span className="notification-time">6 Min Ago</span>
+                        <span className='p-1 bg-success  rounded-circle '></span>
                       </h6>
-                      <p className="noti-details">
-                        {" "}
-                        Sent a amount $210 for his Appointment{" "}
-                        <span className="noti-title">Dr.Maria Dyen</span>
+                      <p style={{
+                        fontWeight: "normal"
+                      }} className="fw-light">
+                        Sent a amount of $210 for his Appointment{" "}
+                        <span className="">Dr.Ruby perin </span>
                       </p>
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className="notification-message">
-                <Link to="/patient/dashboard">
-                  <div className="media d-flex">
-                    {/* <div className="avatar avatar-sm">
-                      <img
-                        className="avatar-img"
-                        alt=""
-                        src={client_04}
-                      />
-                    </div> */}
-                    <div className="media-body">
-                      <h6>
-                        Travis Tremble{" "}
-                        <span className="notification-time">8.30 AM</span>
-                      </h6>
-                      <p className="noti-details"> Send a message to his doctor</p>
+                      <span className="">18.30 PM</span>
                     </div>
                   </div>
                 </Link>
